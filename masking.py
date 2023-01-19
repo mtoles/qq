21516d858578c3a302f21702d2ee1805cff6efa8
destigres <toles.matthew@gmail.com>
2023-01-18 21:24:10 -0500

    feat: masking random sentence

--- masking.py
+++ masking.py
@@ -1,10 +1,11 @@
 from numpy import random
 from utils import dc, tokenizer
 
+
 def mask_random_sentence(example):
     # TODO: mask the input ids instead of the context
-    """Mask random useful sentence in example"""
-    titles = example["context"]["title"]
+    """Mask random useful sentence in example. Return the column entry, not the whole example."""
+    titles = example["context_None"]["title"]
     # create a dictionary mapping each title to its index
     title_to_index = {title: i for i, title in enumerate(titles)}
 
@@ -16,14 +17,20 @@ def mask_random_sentence(example):
     fact_title = example["supporting_facts"]["title"][i]
     fact_title_index = title_to_index[fact_title]
     fact_sent_index = example["supporting_facts"]["sent_id"][i]
-    fact_sent = example["context"]["sentences"][fact_title_index][fact_sent_index]
-    len_fact = len(
-        fact_sent.split()
-    )
+    fact_sent = example["context_None"]["sentences"][fact_title_index][fact_sent_index]
+    len_fact = len(fact_sent.split())
     replacement = " ".join(["[MASK]"] * len_fact)
 
-    example["context"]["sentences"][fact_title_index][fact_sent_index] = replacement
+    example["context_None"]["sentences"][fact_title_index][
+        fact_sent_index
+    ] = replacement
 
-    debug_context = "[SEP]".join([" ".join(x) for x in example["context"]["sentences"]])
+    debug_context = "[SEP]".join(
+        [" ".join(x) for x in example["context_None"]["sentences"]]
+    )
+    return {"masked_col": example["context_None"]}
+    # return example
 
+
+def mask_None(example):
     return example
