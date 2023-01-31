abf502395bcffeb95aa72019e0c586a2aaddafc6
destigres <toles.matthew@gmail.com>
2023-01-31 16:18:32 -0500

    wip: set up classes abstracting model pipelines

--- main.py
+++ main.py
@@ -16,44 +16,60 @@ from dataset_utils import drop_unanswerable
 @click.option("--pm_paths", multiple=True, help="path to primary model")
 @click.option("--pm_arch", multiple=True, help="primary model architecture")
 @click.option("--masking_scheme", help="{randomsentence | None")
-@click.option("--downsample_pt_size", default=None, type=int, help="use at most this many examples in validation")
-def main(pt_dataset_path, pm_paths, pm_arch, masking_scheme, downsample_pt_size):
+@click.option(
+    "--downsample_pt_size",
+    default=None,
+    type=int,
+    help="use at most this many examples in validation",
+)
+@click.option(
+    "--results_file", default=None, help="path to results file. Appended to if exists."
+)
+def main(
+    pt_dataset_path, pm_paths, pm_arch, masking_scheme, downsample_pt_size, results_file
+):
     masking_str = f"fc_{masking_scheme}"
 
     # Unit Tests
     for pma in pm_arch:
         assert pma in ["bigbird", "gpt_neox"]
+
     for model_num, pm_path in enumerate(pm_paths):
         p_model_type = pm_arch[model_num]
-
-        # Load primary model
-        if p_model_type == "bigbird":
-            pm = BigBird_PM(pm_path)
-        elif pm_path == "gpt_neox":
-            raise NotImplementedError
 
         # Receive and prepare the primary task
         pt_dataset = load_from_disk(pt_dataset_path)
         if downsample_pt_size is not None:
-            pt_dataset = pt_dataset.select(range(downsample_pt_size)) 
-        pt_dataset = drop_unanswerable(pt_dataset, masking_scheme=masking_scheme, load_from_cache_file=True)
-        pt_dataset = pm.prepare_data(pt_dataset)
+            pt_dataset = pt_dataset.select(range(downsample_pt_size))
+        # pt_dataset = drop_unanswerable(
+        # pt_dataset, masking_scheme=masking_scheme, load_from_cache_file=True
+        # )
+        # pt_dataset = pm.prepare_data(pt_dataset)
 
-        example = pt_dataset[masking_str][0]
-        print(example)
+        # Load primary model
+        if p_model_type == "bigbird":
+            pm = BigBird_PM(pm_path, raw_val_dataset=pt_dataset)
+        elif pm_path == "gpt_neox":
+            raise NotImplementedError
 
-        pm_output = pm(example)
+        pm.prepare_data(masking_scheme=masking_scheme)
+
+        print
+
+        # example = pt_dataset[masking_str][0]
+        # print(example)
+
+        # pm_output = pm(example)
 
         # perform secondary task
-        secondary_model = Dummy_secondary_model()
-        query = secondary_model(example)
+        # secondary_model = Dummy_secondary_model()
+        # query = secondary_model(example)
 
-        # consult the oracle
-        oracle = Dummy_oracle(pm_output, corpus="This is a test. This is another test. This is a third test.")
-        answer = oracle.consult(query)
+        # # consult the oracle
+        # oracle = Dummy_oracle(pm_output, corpus="This is a test. This is another test. This is a third test.")
+        # answer = oracle.consult(query)
 
         # TODO: Insert answer into the primary task
-        
 
         pass
 
--- primary_models.py
+++ primary_models.py
@@ -3,30 +3,96 @@
 
 from utils import BB_MODEL_ID
 from bb_model import BigBirdForNaturalQuestions
+from utils import collate_fn
+from metrics import compute_metrics
 from transformers import BigBirdTokenizer
 from prepare_data import prepare_inputs_hp
+from transformers import TrainingArguments, Trainer
 
-class Primary_Model():
-    def __init__(self, model_path=None):
-        self.model = None
-        self.tk = None
+
+class Primary_Model:
+    def __init__(
+        self,
+        model_path=None,
+        eval_batch_size=32,
+        raw_val_dataset=None,
+        prepped_val_dataset=None,
+    ):
+        self.model_path = model_path
+        self.eval_batch_size = eval_batch_size
+        self.raw_val_dataset = raw_val_dataset
+        self.prepped_val_dataset = prepped_val_dataset
+        self.collate_fn = lambda x: collate_fn(x, self.tk)
+        self.args = TrainingArguments(
+            output_dir=None,
+            do_train=False,
+            do_eval=True,
+            per_gpu_eval_batch_size=eval_batch_size,
+            group_by_length=True,
+            disable_tqdm=False,
+            remove_unused_columns=False,
+            label_names=[
+                "pooler_label",
+                "start_positions",
+                "end_positions",
+                "gt_answers",
+            ],
+        )
+
     def __call__(self, batch):
         self.forward(batch)
+
     def forward(self, batch):
         self.model(batch)
-    def prepare_data(self, dataset):
-        pass
+
+    def prepare_data(self, masking_scheme):
+        self.trainer = Trainer(
+            model=self.model,
+            args=self.args,
+            data_collator=self.collate_fn,
+            eval_dataset=self.prepped_val_dataset,
+            compute_metrics=lambda x: compute_metrics(x, self.tk, None),
+            tokenizer=self.tk,
+        )
+
 
 class BigBird_PM(Primary_Model):
-    def __init__(self, model_path=None):
+    def __init__(
+        self,
+        model_path=None,
+        eval_batch_size=32,
+        raw_val_dataset=None,
+        prepped_val_dataset=None,
+    ):
         self.tk = BigBirdTokenizer.from_pretrained(BB_MODEL_ID)
         if model_path is None:
-            self.model = BigBirdForNaturalQuestions.from_pretrained(BB_MODEL_ID, self.tk)
+            self.model = BigBirdForNaturalQuestions.from_pretrained(
+                BB_MODEL_ID, self.tk
+            )
         else:
             self.model = BigBirdForNaturalQuestions.from_pretrained(model_path, self.tk)
+        super(BigBird_PM, self).__init__(
+            model_path=model_path,
+            eval_batch_size=eval_batch_size,
+            raw_val_dataset=raw_val_dataset,
+            prepped_val_dataset=prepped_val_dataset,
+        )
+
     def __call__(self, example):
         return self.model(example)
+
     def forward(self, example):
         return self.model(example)
-    def prepare_data(self, dataset):
-        return dataset.map(lambda x: prepare_inputs_hp(x, tk=self.tk, max_length=self.model.bert.embeddings.position_embeddings.weight.shape[0]))
+
+    def prepare_data(self, masking_scheme):
+        self.prepped_val_dataset = self.raw_val_dataset.map(
+            lambda x: prepare_inputs_hp(
+                x,
+                tk=self.tk,
+                max_length=self.model.bert.embeddings.position_embeddings.weight.shape[
+                    0
+                ],
+                masking_scheme=masking_scheme,
+            )
+        )
+        super().prepare_data(self.raw_val_dataset)
